28c0dc028d7476a94a5609d4697b4ab1e396e62e
Matthew Shirtliffe <matt.shirtliffe@gmail.com>
2021-12-07 19:09:47 +0000

    add .env

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/layout/Navbar';
+// import UserItem from './components/layout/users/UserItem';
+import Users from './components/layout/users/Users';
 import './App.css';
+import axios from 'axios';
+
 
 class App extends Component {
 
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount(){
+    this.setState({
+      loading:true
+    })
+    const response = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({
+      loading:false,
+      users: response.data
+    })
+
+  }
+
   render() {
-    const firstName = "Matthew";
-    const lastName = "Shirtliffe";
-    const name = `${firstName} ${lastName}`
-    const loading = true;
-    const showName = true;
-
     return (      
-    <Fragment>
-      {loading ?  <h1>Loading....</h1> :
-      <h1>Hello {showName ? name : 'Stranger'}</h1>}
-    </Fragment>
+    <div className='App'>
+     <Navbar title="Github finder" icon='fab fa-github' /> 
+     <div className='container'>
+      <Users loading={this.state.loading} users={this.state.users} />
+     </div>
+    </div>
     );
-
-    // if(loading){
-    //   return (      
-    //     <h1>Loading....</h1>
-    //     );
-    // }
-
-    // return (      
-    // <Fragment>
-    //   <h1>Hello {name}</h1>
-    // </Fragment>
-    // );
-
-    // {showName ? name : 'Stranger'}
-    // {showName ?? name} cam be
   }
 }
 
